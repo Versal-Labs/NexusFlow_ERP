6ad502e05da1100a27dc269a8cad3f7009dae96f
Mohamed Sakeel <mohamedsakeel@outlook.com>
2026-01-01 23:16:52 +0530

    #23 Create the clean architecture project structre

---
+++ NexusFlow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NexusFlow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
